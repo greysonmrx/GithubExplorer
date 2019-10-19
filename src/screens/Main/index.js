d8ec5d540d32ae75b5defab2937d41ecb4877e2c
greysonmrx <greysonmrx@gmail.com>
2019-10-19 17:24:44 -0300

    Adicionando usuários

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,36 +1,77 @@
 import React, { useEffect, useState } from 'react';
 
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import api from '../../services/api';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import { 
     Container,
     Form,
     Input,
-    SubmitButton
+    SubmitButton,
+    List,
+    User,
+    Avatar,
+    Name,
+    Bio,
+    ProfileButton,
+    ProfileButtonText
 } from './styles';
 
 export default function Main({ navigation }) {
     const [users, setUsers] = useState([]);
     const [newUser, setNewUser] = useState('');
+    const [loading, setLoading] = useState(false);
 
     async function handleAddUser() {
-        const { data } = await api.get(`/users/${newUser}`);
+        setLoading(true);
 
-        const user = {
-            name: data.name,
-            login: data.login,
-            bio: data.bio,
-            avatar: data.avatar_url
-        };
+        try {
+            const { data } = await api.get(`/users/${newUser}`);
 
-        setUsers(lastUsers => [...lastUsers, user]);
-        setNewUser('');
+            console.log(data);
 
-        Keyboard.dismiss();
+            const user = {
+                name: data.name,
+                login: data.login,
+                bio: data.bio,
+                avatar: data.avatar_url
+            };
+
+            setUsers(lastUsers => [...lastUsers, user]);
+            setNewUser('');
+            Keyboard.dismiss();
+
+            setLoading(false);
+        } catch(err) {
+            alert('Usuário não encontrado!');
+            setLoading(false);
+        }
     }
 
+    async function handleUsersUpdate() {
+        await AsyncStorage.setItem('users', JSON.stringify(users));
+        console.log(`Guardou!`);
+    }
+
+    async function getUsers() {
+        const usersStorage = await AsyncStorage.getItem('users');
+
+        if (usersStorage) {
+            setUsers(JSON.parse(usersStorage));
+            console.log(JSON.parse(usersStorage));
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    useEffect(() => {
+        handleUsersUpdate();
+    }, [users]);
+        
     return (
         <Container>
             <Form>
@@ -40,19 +81,46 @@ export default function Main({ navigation }) {
                     placeholder='Adicionar usuário'
                     value={newUser}
                     onChangeText={text => setNewUser(text)}
-                    returnKeyType="send"
+                    returnKeyType='send'
                     onSubmitEditing={handleAddUser}
                 />
                 <SubmitButton
                     onPress={handleAddUser}
+                    loading={loading}
                 >
-                    <Icon 
-                        name='add'
-                        size={20}
-                        color='#FFFFFF'
-                    />
+                    {
+                        loading ? (
+                            <ActivityIndicator 
+                                color='#FFFFFF'
+                            />
+                        ) : (
+                            <Icon 
+                                name='add'
+                                size={20}
+                                color='#FFFFFF'
+                            />
+                        )
+                    }                    
                 </SubmitButton>
             </Form>
+            <List 
+                data={users}
+                keyExtractor={user => user.login}
+                renderItem={({ item }) => (
+                    <User>
+                        <Avatar 
+                            source={{ uri: item.avatar }}
+                        />
+                        <Name>{ item.name }</Name>
+                        <Bio>{ item.bio }</Bio>
+                        <ProfileButton
+                            onPress={() => {}}
+                        >
+                            <ProfileButtonText>Ver perfil</ProfileButtonText>
+                        </ProfileButton>
+                    </User>
+                )}
+            />
         </Container>
     );
 }
